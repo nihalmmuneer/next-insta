ae17aa0d5d27e81a4c0406fb1dc4ad572ba3c0cc
nihal <nihalmmuneer@gmail.com>
2024-04-30 10:48:12 +0530

    Add modal Functionality using react modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { IoAddCircleOutline, IoCamera, IoClose } from "react-icons/io5";
+import { useState } from "react";
+import Modal from "react-modal";
 
 const Header = () => {
   const { data: session } = useSession();
-  console.log(session, "data");
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className=" border-b p-3 shadow-sm">
@@ -34,23 +37,51 @@ const Header = () => {
           placeholder="Search..."
           className="border py-3 px-3 border-gray-200 rounded-lg"
         />
-        {/* button */}
-        {session ? (
-          <img
-            src={session.user.image}
-            alt={session.user.name}
-            className="w-10 h-10 rounded-full cursor-pointer"
-            onClick={() => signOut()}
+        <div className="flex items-center gap-4">
+          <IoAddCircleOutline
+            onClick={() => setIsOpen(true)}
+            className="w-8 h-8 hover:text-red-500 cursor-pointer hover:scale-125 transition duration-300"
           />
-        ) : (
-          <button
-            onClick={() => signIn()}
-            className="text-blue-400 text-sm font-bold cursor-pointer"
-          >
-            Log In
-          </button>
-        )}
+          {/* button */}
+          {session ? (
+            <img
+              src={session.user.image}
+              alt={session.user.name}
+              className="w-10 h-10 rounded-full cursor-pointer"
+              onClick={() => signOut()}
+            />
+          ) : (
+            <button
+              onClick={() => signIn()}
+              className="text-blue-400 text-sm font-bold cursor-pointer"
+            >
+              Log In
+            </button>
+          )}
+        </div>
       </div>
+      <Modal
+        isOpen={isOpen}
+        className="w-[90%] mx-auto outine-none absolute top-56 left-[50%] p-6  translate-x-[-50%] max-w-lg rounded-md shadow-lg"
+        onRequestClose={() => setIsOpen(false)}
+        // ariaHideApp={false}
+      >
+        <div className="flex flex-col justify-center  items-center">
+          <IoCamera className="text-5xl text-center justify-center text-slate-400" />
+          <input
+            type="text"
+            placeholder="Please enter your caption..."
+            className="p-2 focus:outline-none"
+          />
+          <button className="disabled:bg-gray-100 disabled:cursor-not-allowed cursor-pointer bg-red-700 hover:bg-red-500 font-semibold w-full p-2 rounded-lg text-white uppercase mt-2">
+            Upload Post
+          </button>
+          <IoClose
+            className="absolute top-2 right-2 text-xl cursor-pointer hover:text-red-500 trasition duration-300"
+            onClick={() => setIsOpen(false)}
+          />
+        </div>
+      </Modal>
     </div>
   );
 };
