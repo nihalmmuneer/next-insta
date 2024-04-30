77b0d85e44dbe47c1fe14e4ff4987c02222f318a
nihal <nihalmmuneer@gmail.com>
2024-04-30 16:06:24 +0530

    complete mini Profile component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,7 +3,7 @@ import Posts from "./Posts";
 
 const Feed = () => {
   return (
-    <main className="grid grid-cols-1 md:grid-cols-3">
+    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
     {/* Post */}
       <section className="md:col-span-2">
         <Posts />
--- src/components/MiniPosts.jsx
+++ src/components/MiniPosts.jsx
@@ -1,8 +1,40 @@
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const MiniPosts = () => {
+  const { data: session } = useSession();
   return (
-    <div>MiniPosts</div>
-  )
-}
+    <div className="flex items-center mt-16 justify-between w-[280px]">
+      <div className="flex gap-2 items-center justify-center">
+        <img
+          src={session?.user?.image || "insta-logo.webp"}
+          alt="profileImg"
+          className="w-12 h-12 rounded-full"
+        />
+        <div>
+          {session && <h2 className="font-bold">{session?.user?.username}</h2>}
+          <h1 className="text-gray-400 whitespace-nowrap text-sm">
+            Welcome to Instagram
+          </h1>
+        </div>
+      </div>
+      {session ? (
+        <button
+          onClick={() => signOut()}
+          className="font-extrabold text-blue-400 text-sm cursor-pointer whitespace-nowrap "
+        >
+          Sign Out
+        </button>
+      ) : (
+        <button
+          className="font-bold text-blue-500 cursor-pointer text-sm"
+          onClick={() => signIn()}
+        >
+          Log In
+        </button>
+      )}
+    </div>
+  );
+};
 
-export default MiniPosts
+export default MiniPosts;
